becbc681437fc78271695096128ebbebc390c361
AmandaCrispim <amandacrispim384@gmail.com>
2022-08-22 14:26:51 -0300

    API Rest do projeto Garagem

--- core/views.py
+++ core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Carro, Marca
+from core.serializers import CategoriaSerializer, MarcaSerializer, CarroSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return CategoriaDetailSerializer
+        return CategoriaSerializer
+
+
+class CarroViewSet(ModelViewSet):
+    queryset = Carro.objects.all()
+    serializer_class = CarroSerializer
+def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return CarroDetailSerializer
+        return CarroSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return MarcaDetailSerializer
+        return MarcaSerializer
+
+   
--- garagem/urls.py
+++ garagem/urls.py
@@ -5,3 +5,11 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from core.views import CategoriaViewSet, CarroViewSet, MarcaViewSet
+
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'Carro', CarroViewSet)
+router.register(r'Marca', MarcaViewSet)
+
+...
